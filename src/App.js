4a60d97e4e828e4f86a2d652cb0b6584d8d965ee
Austin Lords <austinlords@outlook.com>
2019-07-21 22:30:35 -0700

    add http service module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+import config from "./config.json";
 
 class App extends Component {
   state = {
@@ -10,28 +9,35 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndPoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndPoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async post => {
-    post.title = "Updated";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    const originalPosts = this.state.posts;
 
+    post.title = "Updated";
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...post };
     this.setState({ posts });
+
+    try {
+      await http.put(config.apiEndPoint + "/" + post.id, post);
+    } catch (ex) {
+      alert("could not update");
+
+      this.setState({ posts: originalPosts });
+    }
   };
 
-  // Optimistic Update for fast UI
   handleDelete = async post => {
     const originalPosts = this.state.posts;
 
@@ -39,10 +45,11 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
-      throw new Error("");
-    } catch (err) {
-      alert("something failed while deleting a post");
+      await http.delete(config.apiEndPoint + "/" + post.id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        alert("This post has already been deleted");
+
       this.setState({ posts: originalPosts });
     }
   };
